f436319fc0681ed47c23a7b328e27ba7762734b5
wtompals <wtompals@capgemini.com>
2020-05-12 12:00:06 +0200

    Add routing to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from './modules/dashboard/components/dashboard-page/dashboard-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
