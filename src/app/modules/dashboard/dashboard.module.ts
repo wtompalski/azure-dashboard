0789a22ce8e8d4a211ac5c6512c4acec57dc3359
wtompals <wtompals@capgemini.com>
2020-05-12 11:59:36 +0200

    Add dashboard main page

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { WeatherComponent } from './components/weather/weather.component';
 import { StocksComponent } from './components/stocks/stocks.component';
 import { ExchangeRatesComponent } from './components/exchange-rates/exchange-rates.component';
+import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
 
 
 
 @NgModule({
-  declarations: [WeatherComponent, StocksComponent, ExchangeRatesComponent],
+  declarations: [WeatherComponent, StocksComponent, ExchangeRatesComponent, DashboardPageComponent],
   imports: [
     CommonModule
   ]
