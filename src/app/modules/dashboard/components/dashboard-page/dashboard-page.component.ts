b592e5fb0a34291f6070b97b26493cdbcecac827
wtompals <wtompals@capgemini.com>
2020-05-13 08:17:38 +0200

    Use weather component

--- src/app/modules/dashboard/components/dashboard-page/dashboard-page.component.ts
+++ src/app/modules/dashboard/components/dashboard-page/dashboard-page.component.ts
@@ -1,39 +1,9 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { map, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.css']
 })
-export class DashboardPageComponent implements OnDestroy {
-
-  private unsubscribe$ = new Subject<void>();
-
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Weather', cols: 1, rows: 1 },
-          { title: 'Stocks', cols: 1, rows: 1 },
-          { title: 'Exchange Rates', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Weather', cols: 2, rows: 1 },
-        { title: 'Stocks', cols: 1, rows: 1 },
-        { title: 'Exchange Rates', cols: 1, rows: 1 }
-      ];
-    }), takeUntil(this.unsubscribe$)
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+export class DashboardPageComponent {
 }
