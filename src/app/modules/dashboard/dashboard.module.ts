2322d1a1030e46ed4932c4cfb2827fff62974c1e
wtompals <wtompals@capgemini.com>
2020-05-13 10:00:07 +0200

    Add missing mat list module

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -13,6 +13,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatMenuModule,
     MatToolbarModule,
+    MatListModule
   ],
 })
 export class DashboardModule {}
