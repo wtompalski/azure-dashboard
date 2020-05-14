89b61689845e6ef6db346f3e99d0feabcea0a73d
wtompals <wtompals@capgemini.com>
2020-05-14 15:02:53 +0200

    Add debug page

--- server/index.js
+++ server/index.js
@@ -4,27 +4,42 @@ const proxy = require("express-http-proxy");
 const publicweb = process.env.PUBLICWEB || ".";
 const app = express();
 
+const weatherApi = process.env.WEATHER_API || "http://api.weatherapi.com";
+const exchangeApi = process.env.EXCHANGE_API || "www.ecb.europa.eu";
+const stocksApi = process.env.STOCKS_API || "finnhub.io";
+
+const port = process.env.PORT || "3000";
+
 app.use(express.static(publicweb));
-app.use("/api/weather", proxy("http://api.weatherapi.com"));
+app.use("/api/weather", proxy(weatherApi));
 app.use(
   "/api/exchange",
-  proxy("www.ecb.europa.eu", {
+  proxy(exchangeApi, {
     https: true,
   })
 );
 app.use(
   "/api/stocks",
-  proxy("finnhub.io", {
+  proxy(stocksApi, {
     https: true,
   })
 );
 
-console.log(`serving ${publicweb}`);
+app.get("/admin", (req, res) => {
+  res.send(
+    `PORT: ` +
+      port +
+      `<br>WEATHER_API: ` +
+      weatherApi +
+      `<br>EXCHANGE_API: ` +
+      exchangeApi +
+      `<br>STOCKS_API: ` +
+      stocksApi
+  );
+});
 
 app.get("*", (req, res) => {
   res.sendFile(`index.html`, { root: publicweb });
 });
-
-const port = process.env.PORT || "3000";
 
 app.listen(port, () => console.log(`API running on localhost:${port}`));
