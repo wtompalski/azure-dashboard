c0469f0430ce69b12fa9e743a04bda6f24c9262a
wtompals <wtompals@capgemini.com>
2020-05-14 15:05:17 +0200

    Set dashboard as default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardPageComponent } from './modules/dashboard/components/dashboard-page/dashboard-page.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
   { path: 'dashboard', component: DashboardPageComponent },
 ];
 
