7fe2b5d22ce89abebb1bccfb1469bae29e377d31
wtompals <wtompals@capgemini.com>
2020-05-14 13:24:31 +0200

    Add proxy to node server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const proxy = require("express-http-proxy");
 
 const publicweb = process.env.PUBLICWEB || ".";
 const app = express();
 
 app.use(express.static(publicweb));
+app.use("/api/weather", proxy("http://api.weatherapi.com/v1/current.json"));
 
 console.log(`serving ${publicweb}`);
 
