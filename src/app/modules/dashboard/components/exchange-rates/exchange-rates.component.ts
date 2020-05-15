2841ccd1b123412698dbdd84319b1cc12646489e
wtompals <wtompals@capgemini.com>
2020-05-15 07:09:59 +0200

    Implement sorting for currency rates

--- src/app/modules/dashboard/components/exchange-rates/exchange-rates.component.ts
+++ src/app/modules/dashboard/components/exchange-rates/exchange-rates.component.ts
@@ -1,34 +1,60 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { ExchangeRatesDataSource } from './exchange-rates.datasource';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { ExchangeRate } from '../../models/exchange-rate';
 import { DashboardService } from '../../dashboard.service';
+import { tap, finalize, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-exchange-rates',
   templateUrl: './exchange-rates.component.html',
   styleUrls: ['./exchange-rates.component.css'],
 })
-export class ExchangeRatesComponent implements AfterViewInit, OnInit {
+export class ExchangeRatesComponent
+  implements AfterViewInit, OnInit, OnDestroy {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<ExchangeRate>;
-  dataSource: ExchangeRatesDataSource;
+  dataSource: MatTableDataSource<ExchangeRate>;
+  loading = false;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(private dashboardService: DashboardService) {}
 
   displayedColumns = ['flag', 'currency', 'rate'];
 
   ngOnInit() {
-    this.dataSource = new ExchangeRatesDataSource(this.dashboardService);
+    this.dataSource = new MatTableDataSource<ExchangeRate>();
+
+    this.loading = true;
+
+    this.dashboardService
+      .getExchangeRates()
+      .pipe(
+        tap((result) => (this.dataSource.data = result)),
+        finalize(() => (this.loading = false)),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   getSymbol(exchangeRate: ExchangeRate): string {
